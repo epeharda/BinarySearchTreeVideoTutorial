9ba971566f389b512de88b60bf2e9890dd6b3114
epeharda <ericapeharda@gmail.com>
2015-12-03 14:01:36 -0700

    setting up project

---
+++ BST.h
@@ -0,0 +1,17 @@
+class BST
+{
+//only accessible to class itself
+private:
+	struct node
+	{
+		int key;
+
+
+	};
+
+//accesible everywhere - main.cpp can see this
+public:
+
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "BST.cpp"
+
+using namespace std;
+
+int main()
+{
+public:
+
+private:
+
+
+
+	return 0;
+}
